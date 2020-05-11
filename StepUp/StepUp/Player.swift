ed92d8763ec57661a0eb27e01fc7cc8ab24cd710
Zach Grande <62479938+ZachGrande@users.noreply.github.com>
2020-05-11 16:29:31 -0700

    Created player object

---
+++ StepUp/StepUp/Player.swift
@@ -0,0 +1,19 @@
+//
+//  Player.swift
+//  StepUp
+//
+//  Created by Zach Grande on 5/11/20.
+//  Copyright © 2020 Zach Grande. All rights reserved.
+//
+
+import UIKit
+
+class Player: NSObject {
+    // Fields
+    
+    // Constructors
+    
+    // Modifier methods
+    
+    // Instance methods
+}
