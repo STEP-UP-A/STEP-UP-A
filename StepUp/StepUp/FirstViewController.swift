8a16509af140fd269456158da7c3d26ff7950373
ehummel99 <elanah852@gmail.com>
2020-05-11 18:12:18 -0700

    started objectives screen with user input box

--- StepUp/StepUp/FirstViewController.swift
+++ StepUp/StepUp/FirstViewController.swift
@@ -2,19 +2,35 @@
 //  FirstViewController.swift
 //  StepUp
 //
+//  This is the user step input screen loacted on the objectives screen
+//
 //  Created by Zach Grande on 4/20/20.
 //  Copyright © 2020 Zach Grande. All rights reserved.
 //
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var stepInputTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // set up textfield delegate
+        stepInputTextField.delegate = self
+        
     }
-
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // hide keyboard
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        // do something with input text
+    }
 
 }
 
