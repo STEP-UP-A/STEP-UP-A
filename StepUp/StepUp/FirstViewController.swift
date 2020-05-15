c707a5cc594ebc12f0c3ccec2d72ce5f68f267c8
ehummel99 <elanah852@gmail.com>
2020-05-14 18:34:30 -0700

    Adding a label above the input field displaying the text fields input (where you would input the step count).Adds the number you input in the text field to the steps in the label

--- StepUp/StepUp/FirstViewController.swift
+++ StepUp/StepUp/FirstViewController.swift
@@ -13,9 +13,54 @@ import UIKit
 class FirstViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var stepInputTextField: UITextField!
+    @IBOutlet weak var stepCountLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+
+//        //Background
+//        view.frame = CGRect(x: 0, y: 0, width: 375, height: 812)
+//
+//        self.view.sendSubviewToBack(view)
+//view.backgroundColor = .white
+//
+//
+//let layer0 = CALayer()
+//
+//layer0.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1).cgColor
+//
+//layer0.bounds = view.bounds
+//
+//layer0.position = view.center
+//
+//view.layer.addSublayer(layer0)
+//
+//
+//let layer1 = CAGradientLayer()
+//
+//layer1.colors = [
+//
+//  UIColor(red: 0.176, green: 0.612, blue: 0.859, alpha: 0.5).cgColor,
+//
+//  UIColor(red: 0.608, green: 0.318, blue: 0.878, alpha: 0).cgColor
+//
+//]
+//
+//layer1.locations = [0, 1]
+//
+//layer1.startPoint = CGPoint(x: 0.25, y: 0.5)
+//
+//layer1.endPoint = CGPoint(x: 0.75, y: 0.5)
+//
+//layer1.transform = CATransform3DMakeAffineTransform(CGAffineTransform(a: 0, b: 1, c: -1, d: 0, tx: 1, ty: 0))
+//
+//layer1.bounds = view.bounds.insetBy(dx: -0.5*view.bounds.size.width, dy: -0.5*view.bounds.size.height)
+//
+//layer1.position = view.center
+//
+//view.layer.addSublayer(layer1)
         
         // set up textfield delegate
         stepInputTextField.delegate = self
@@ -30,7 +75,17 @@ class FirstViewController: UIViewController, UITextFieldDelegate {
     
     func textFieldDidEndEditing(_ textField: UITextField) {
         // do something with input text
+        if(stepCountLabel.text == nil){
+            stepCountLabel.text = stepInputTextField.text
+        }
+        else {
+        stepCountLabel.text = String(    (stepInputTextField.text as! NSString).integerValue + (stepCountLabel.text as! NSString).integerValue)
+        }
+
+        
     }
+    
+
 
 }
 
